470e1d145fcc612ee6a3ce82be88aaeb55865d00
Cezary Siłuszyk <cs321191@students.mimuw.edu.pl>
2014-10-12 01:17:17 +0200

    Add subtle grayscale style.

--- location_field/static/location_field/js/form.js
+++ location_field/static/location_field/js/form.js
@@ -23,7 +23,9 @@
             var point = new google.maps.LatLng(1, 1);
 
             var options = {
-                mapTypeId: google.maps.MapTypeId.ROADMAP
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                // Source: http://snazzymaps.com/style/15/subtle-grayscale
+                style: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
             };
             
             location_map = new google.maps.Map(map[0], options);
